4135953b9d884335328d5a552fa275d4092e565e
Eldar <eldarba@gmail.com>
2025-06-18 20:39:04 +0300

    auto commit batch

--- auto_add_comnit_push.bat
+++ auto_add_comnit_push.bat
@@ -1,9 +1,9 @@
-echo about to start commiting all changes. press CTRL + C to abort
-pause
+rem echo about to start commiting all changes. press CTRL + C to abort
+rem pause
 git pull origin
 git status
 git add --all
 git commit -m "auto commit batch"
 git push origin
 git status
-pause
+rem pause
